012f4d55e722b5821008b6716e18f242c1d86704
vinci <tuyendt@itsol.vn>
2023-03-07 18:35:14 +0700

    update roleUser entity

--- src/main/java/com/anchoi/models/RoleUser.java
+++ src/main/java/com/anchoi/models/RoleUser.java
@@ -1,15 +1,22 @@
 package com.anchoi.models;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @Entity
-@EqualsAndHashCode(callSuper=false)
 @Table(name = "role_user", schema = "an_choi_app", catalog = "")
-public class RoleUser extends BaseEntity {
+public class RoleUser {
+    @Id
+    @Column(name = "id", nullable = true)
+    private UUID id;
+
     @Basic
     @Column(name = "role_id")
     private String roleId;
@@ -17,4 +24,16 @@ public class RoleUser extends BaseEntity {
     @Column(name = "user_id")
     private String userId;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleUser roleUser = (RoleUser) o;
+        return Objects.equals(roleId, roleUser.roleId) && Objects.equals(userId, roleUser.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
